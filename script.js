09cec8988865311249d76955fade589c21e94948
PRAVIN <pravinonline05@gmail.com>
2023-03-30 14:22:58 +0530

    Add all query selectors

---
+++ script.js
@@ -0,0 +1,21 @@
+const inputSlider = document.querySelector("[data-lengthSlider]");
+const lengthDisplay = document.querySelector("[data-lengthNumber]");
+const passwordDisplay = document.querySelector("[data-passwordDisplay]");
+const copybtn = document.querySelector("[data-copy]");
+const copyMsg = document.querySelector("[data-copyMsg]");
+const uppercaseCheck = document.querySelector("#uppercase");
+const lowercaseCheck = document.querySelector("#lowercase");
+const numbersCheck = document.querySelector("#numbers");
+const symbolsCheck = document.querySelector("#symbols");
+const indicator= document.querySelector("[data-indicator]");
+const generateBtn= document.querySelector(".generateButton");
+const allCheckBox= document.querySelector("input[type=checkbox]");
+
+
+
+
+
+
+
+
+
