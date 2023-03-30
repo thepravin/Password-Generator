fc33997bb0b36921959e992f7e228b5ce5cc8912
PRAVIN <pravinonline05@gmail.com>
2023-03-30 15:07:23 +0530

    create Strength checker

--- script.js
+++ script.js
@@ -7,45 +7,68 @@ const uppercaseCheck = document.querySelector("#uppercase");
 const lowercaseCheck = document.querySelector("#lowercase");
 const numbersCheck = document.querySelector("#numbers");
 const symbolsCheck = document.querySelector("#symbols");
-const indicator= document.querySelector("[data-indicator]");
-const generateBtn= document.querySelector(".generateButton");
-const allCheckBox= document.querySelector("input[type=checkbox]");
+const indicator = document.querySelector("[data-indicator]");
+const generateBtn = document.querySelector(".generateButton");
+const allCheckBox = document.querySelector("input[type=checkbox]");
 const symbols = '~`!@#$%^&*()_-+={[}];:"<,>.?/';
 
-let password ="";
+let password = "";
 let passwordLength = 10;
 let checkCount = 1;
 hadleSlider();
 // set strength circle color to gray
 
 // set password length
-function hadleSlider(){
-    inputSlider.value= passwordLength;
+function hadleSlider() {
+    inputSlider.value = passwordLength;
     lengthDisplay.innerText = passwordLength;
 }
 
-
-function setIndicator(color){
+function setIndicator(color) {
     indicator.getElementsByClassName.backgroundColor = color;
     // shadow
 }
 
-function getRndInteger(min,max){
-  return Math.floor(Math.random()*(max-min))+min;
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateRndNumber(){
-    return getRndInteger(0,9);
+function generateRndNumber() {
+    return getRndInteger(0, 9);
 }
 
-function generateLowerCase(){
-    return getRndInteger(97,123); // ASCI values of lowercase
+function generateLowerCase() {
+    return getRndInteger(97, 123); // ASCI values of lowercase
 }
-function generateUpperCase(){
-    return getRndInteger(65,91); // ASCI values of uppercase
+function generateUpperCase() {
+    return getRndInteger(65, 91); // ASCI values of uppercase
 }
 
-function generateSymbols(){
-    const randNum = getRndInteger(0,symbols.length);
+function generateSymbols() {
+    const randNum = getRndInteger(0, symbols.length);
     return symbols.charAt(randNum);
+}
+
+function calcStrength() {
+    let hasUpper = false;
+    let hasLower = false;
+    let hasNum = false;
+    let hasSym = false;
+
+    if (uppercaseCheck.checked) hasUpper = true;
+    if (lowercaseCheck.checked) hasLower = true;
+    if (numbersCheck.checked) hasNum = true;
+    if (symbolsCheck.checked) hasSym = true;
+
+    if (hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8) {
+        setIndicator("#0f0");
+    } else if (
+        (hasLower || hasUpper) &&
+        (hasNum || hasSym) &&
+        passwordLength >= 6
+    ) {
+        setIndicator("#ff0");
+    } else {
+        setIndicator("#f00");
+    }
 }
